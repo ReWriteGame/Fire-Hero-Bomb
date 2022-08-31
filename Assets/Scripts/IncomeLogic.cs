d2d4cde802ad097b8f06dfe04f07ffd2a50c0ccb
ReWriteGame <ReWriteGame>
2022-08-31 14:08:37 +0300

    add logic income

---
+++ Assets/Scripts/IncomeLogic.cs
@@ -0,0 +1,20 @@
+using Game.Logic.Score;
+using TMPro;
+using UnityEngine;
+
+public class IncomeLogic : MonoBehaviour
+{
+  [SerializeField] private Money money;
+  [SerializeField] private ScoreCounter score;
+  [SerializeField] private LevelCounter income;
+  [SerializeField] private TextMeshProUGUI labelIncome;
+  [SerializeField] private TextMeshProUGUI labelIncome2;
+
+  public void EndGame()
+  {
+    int addValue = (int)(score.Score * income.GetCurrentValue());
+    money.Add(addValue);
+    labelIncome.text = $"{addValue}";
+    labelIncome2.text = $"{addValue}";
+  }
+}
